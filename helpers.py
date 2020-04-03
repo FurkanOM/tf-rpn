c8b3854e6893c5c6bc81ce054f13b06f04babde2
FurkanOM <furkanomerustaoglu@gmail.com>
2020-04-03 10:57:10 +0300

    required package imported

--- helpers.py
+++ helpers.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 ###########################################
 ## Pascal VOC
